60ca5f951c9e79f7500a932757c180ccb2c9219a
Alessio Stalla <alessiostalla@gmail.com>
2020-10-06 08:56:15 +0200

    Integrate autocompletion, remove unnecessary code

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -17,9 +17,7 @@ let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
+	let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
 	// The debug options for the server
 	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
 	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
@@ -47,8 +45,8 @@ export function activate(context: ExtensionContext) {
 
 	// Create the language client and start the client.
 	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
+		'sampleKotlinCompletionServer',
+		'Example of a Kotlin code-completion server',
 		serverOptions,
 		clientOptions
 	);
